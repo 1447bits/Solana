9183a1c1bbba2f7ac300c99f5d36d1186a734b50
Atharv Vyas <untilinfinity00@gmail.com>
2022-11-25 10:35:34 +0000

    Create hello.js

---
+++ hello.js
@@ -0,0 +1,34 @@
+// // try catch !!
+// try {
+//   const a = 10;
+//   const b = 11;
+//   if (a == b) {
+//     console.log("continue to code");
+//   } else {
+//     console.log("a!=b");
+//   }
+// } catch (err) {
+//   console.log(err);
+// }
+
+// _____________________________________________________
+
+// function name
+
+// function sample(a, b) {
+//   return a + b;
+// }
+
+// console.log("22 + 33 = ", sample(12, 33));
+
+// _____________________________________________________
+
+// map
+// const x = [1,2,3,4,5,6,7]
+// const func = x.map(a) => {
+//     console.log(a)
+// }
+// Not working dont know why
+// _____________________________________________________
+
+// _____________________________________________________
